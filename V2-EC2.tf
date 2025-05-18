739b7226b90e395714eade081263ed6b36c2459d
mohanjay112 <mohanjay112@gmail.com>
2025-05-18 21:38:24 +0530

    intial comment V2-EC2.tf

---
+++ V2-EC2.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+    region = "us-east-1"  
+}
+
+resource "aws_instance" "demo_server" {
+    ami = "ami-0953476d60561c955"
+    instance_type = "t2.micro"
+    key_name   = "Demo_key_pair"
+    security_groups = [ "demo-sg" ]
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "ssh access"
+
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+  }
+}
