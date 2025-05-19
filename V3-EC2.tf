a7c888ac68966b83a10c42c26fb70881fddf74fa
mohanjay112 <mohanjay112@gmail.com>
2025-05-19 17:26:13 +0530

    Add jenkins

--- V3-EC2.tf
+++ V3-EC2.tf
@@ -21,9 +21,16 @@ resource "aws_security_group" "demo-sg" {
   vpc_id = aws_vpc.dpp-vpc.id
 
   ingress {
-    description = "ssh"
+    description = "SSH"
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Jenkins port"
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
